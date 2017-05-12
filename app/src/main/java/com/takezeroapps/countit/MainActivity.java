f807e18fd18e69e714b4ec8c416231c3b62e975a
bclai7 <bclai27@gmail.com>
2017-05-11 22:13:48 -0400

    init commit

---
+++ app/src/main/java/com/takezeroapps/countit/MainActivity.java
@@ -0,0 +1,266 @@
+package com.takezeroapps.countit;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.takezeroapps.countit.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    boolean isNegative;
+    int newNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.plusButton);
+        Button subButton = (Button) findViewById(R.id.minusButton);
+        ImageButton resetButton = (ImageButton) findViewById(R.id.resetButton);
+        ImageButton counterButton = (ImageButton) findViewById(R.id.countButton);
+        final OperatorFragment opf = new OperatorFragment();
+
+        counterButton.setOnLongClickListener(
+                new ImageButton.OnLongClickListener(){
+                    @Override
+                    public boolean onLongClick(final View view) {
+                        final int currcount = opf.getCount();
+
+                        final CharSequence[] negOptions = {MainActivity.this.getResources().getString(R.string.make_negative_num)}; //choices to select from, only one choice so it only has one element
+                        final ArrayList selectedItems=new ArrayList();
+
+
+                        final AlertDialog.Builder counterChanger = new AlertDialog.Builder(MainActivity.this);
+                        counterChanger.setTitle(R.string.change_count); //set title
+
+                        // Set up the input
+                        final EditText input = new EditText(MainActivity.this);
+
+                        // Specify the type of input expected; this, for example, sets the input as a number, and will use the numpad
+                        input.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_NORMAL);
+                        //input.setText(Integer.toString(opf.getCount())); //sets default text to the current number
+                        //input.setSelection((input.getText().toString().length())); //sets index at end of text
+                        //input.selectAll(); //selects all text entered
+                        counterChanger.setView(input);
+                        //newNum=opf.getCount();
+
+                        // Set up the buttons
+                        counterChanger.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                String input_string = input.getText().toString().trim();
+
+                                if(input_string.isEmpty() || input_string.length() == 0 || input_string.equals("") || TextUtils.isEmpty(input_string)) //check if input is empty
+                                {
+                                    Snackbar.make(view, R.string.no_input_message, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                                    newNum=opf.getCount(); //set new count back to old count (or else manually setting a real number > resetting count > entering blank input = count being the original real number instead of 0 after the reset)
+                                    dialog.cancel();
+                                }
+                                else //if string is not empty, convert to int
+                                    newNum = Integer.valueOf(input.getText().toString());//get integer value of new number
+
+                                if(newNum > 2147483646) //if entered is too high, print error and return to original number
+                                {
+                                    Snackbar.make(view, R.string.too_high_message, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                                    dialog.cancel();
+                                }
+                                else if(newNum < -2147483646) //if number entered is too low, print error and return to original number
+                                {
+                                    Snackbar.make(view, R.string.too_high_message, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                                    dialog.cancel();
+                                }
+                                else //else change number
+                                {
+                                    if(isNegative)
+                                        opf.changeCount(-1*newNum); //if isNegative checkbox is checked, make the number negative
+                                    else
+                                        opf.changeCount(newNum); //if checkbox is not checked, keep number the same
+                                }
+                            }
+                        });
+                        counterChanger.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel(); //cancel dialog and do not save changes when "cancel" button is clicked
+                            }
+                        });
+                        counterChanger.setMultiChoiceItems(negOptions, null, new DialogInterface.OnMultiChoiceClickListener() { //checkbox for negative number
+                            @Override
+                            public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                                if (isChecked) {
+                                    isNegative=true; //if checkbox is checked, set the boolean value for negative number as true
+                                }
+                                else
+                                    isNegative=false; //otherwise if checkbox is not checked, then keep value positive
+                            }
+                        });
+
+                        counterChanger.show(); //show dialog
+                        isNegative=false; //sets negative flag back to false after dialog is closed. This is so the input doesn't stay negative on each new change by the user
+
+                        return true;
+                    }
+                }
+        );
+
+        //When the PLUS button is pressed, increment by 1
+        addButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        int currAddCount=opf.getCount();
+                        if((currAddCount + 1)>2147483646) //if next number would be higher than max print error
+                        {
+                            Snackbar.make(v, R.string.max_num, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                        }
+                        else //else add to count
+                            opf.addCount();
+                    }
+                }
+        );
+        subButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        int currSubCount=opf.getCount();
+                        if((currSubCount - 1) < -2147483647) //if next number would be lower than min print error
+                        {
+                            Snackbar.make(v, R.string.min_num, Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                        }
+                        else //else decrement
+                            opf.subCount();
+                    }
+                }
+        );
+        resetButton.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        // Instantiate an AlertDialog.Builder with its constructor
+                        AlertDialog.Builder resetDialog = new AlertDialog.Builder(MainActivity.this);
+
+                        // Set Dialog Title, message, and other properties
+                        resetDialog.setMessage(R.string.reset_question)
+                                .setTitle(R.string.reset_title)
+                        ; // semi-colon only goes after ALL of the properties
+
+                        // Add the buttons
+                        resetDialog.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // reset count if "yes" is clicked
+                                opf.resetCount();
+                            }
+                        });
+                        resetDialog.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //cancel dialog if "no" is clicked
+                                dialog.cancel();
+                            }
+                        });
+
+                        // Get the AlertDialog from create()
+                        AlertDialog dialog = resetDialog.create();
+
+                        //show dialog when reset button is clicked
+                        resetDialog.show();
+                    }
+                }
+        );
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.d("test", "Setting dots Pressed");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Go to home/main activity
+            Log.d("test", "Nav Pressed");
+            //startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        } else if (id == R.id.nav_multicounter) {
+            //go to multicounter
+
+        } else if (id == R.id.nav_settings) {
+            //go to settings
+
+        } else if (id == R.id.nav_share) {
+            //let users share app
+
+        } else if (id == R.id.nav_rate) {
+            //go to app page in google store
+
+        } else if (id == R.id.nav_contact) {
+            //let users contact through email
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/takezeroapps/countit/OperatorFragment.java
@@ -0,0 +1,76 @@
+package com.takezeroapps.countit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.takezeroapps.countit.R;
+
+/**
+ * Created by scoob on 1/7/2017.
+ */
+public class OperatorFragment extends Fragment{
+
+    private static TextView cnt;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.counter_fragment, container, false);
+        cnt = (TextView) view.findViewById(R.id.count);
+        return view;
+    }
+
+    public int getCount() //this function is called by the main activity to get the current count
+    {
+        return Integer.valueOf(cnt.getText().toString());
+    }
+    public void changeCount(int num) //This function is used by the main activity to change the count when the button is held
+    {
+        Operations op = new Operations(); //create object from Operations.java class
+        int size = op.getSize(num); //use method to get size of text
+        cnt.setTextSize(TypedValue.COMPLEX_UNIT_SP, size); //sets the text size in sp units depending on the size of the number
+        String countstring = Integer.toString(num); //convert the input number to a string
+        cnt.setText(countstring); //sets text to specified string
+    }
+
+    public void addCount()
+    {
+        int num = Integer.valueOf(cnt.getText().toString());
+        num++;
+        Operations op = new Operations(); //create object from Operations.java class
+        int size = op.getSize(num); //use method to get size of text
+        cnt.setTextSize(TypedValue.COMPLEX_UNIT_SP, size); //sets the text size in sp units depending on the size of the number
+        String countstring = Integer.toString(num);
+        cnt.setText(countstring);
+    }
+    public void subCount()
+    {
+        int num = Integer.valueOf(cnt.getText().toString());
+        num--;
+        Operations op = new Operations(); //create object from Operations.java class
+        int size = op.getSize(num); //use method to get size of text
+        cnt.setTextSize(TypedValue.COMPLEX_UNIT_SP, size); //sets the text size in sp units depending on the size of the number
+        String countstring = Integer.toString(num);
+        cnt.setText(countstring);
+    }
+    public void resetCount()
+    {
+        changeCount(0);
+        /*
+        int num = Integer.valueOf(cnt.getText().toString());
+        num=0;
+        Operations op = new Operations(); //create object from Operations.java class
+        int size = op.getSize(num); //use method to get size of text
+        cnt.setTextSize(TypedValue.COMPLEX_UNIT_SP, size); //sets the text size in sp units depending on the size of the number
+        String countstring = Integer.toString(num);
+        cnt.setText(countstring);
+        */
+    }
+
+}
