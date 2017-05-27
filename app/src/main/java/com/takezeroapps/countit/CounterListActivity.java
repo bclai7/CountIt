379c952ebe266cf52f9db5e157b4b4a4c0d06d1a
bclai7 <bclai27@gmail.com>
2017-05-27 13:58:00 -0400

    onClick and onLongClick listeners for multi-counter list view items added

--- app/src/main/java/com/takezeroapps/countit/CounterListActivity.java
+++ app/src/main/java/com/takezeroapps/countit/CounterListActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -78,8 +79,54 @@ public class CounterListActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //showRoute(position);
-                Log.d("test", "item clicked");
+                Log.d("test","Tapped:  " + ((TextView) view).getText());
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //on long click
+
+                Log.d("test","Long clicked:  " + ((TextView) view).getText());
+                final String item = (String) ((TextView) view).getText();
+
+                //create popup dialog
+                String names[] ={getResources().getString(R.string.open_counter), getResources().getString(R.string.rename_counter), getResources().getString(R.string.delete_counter), getResources().getString(R.string.details_counter), };
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(CounterListActivity.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View convertView = (View) inflater.inflate(R.layout.popup_list, null);
+                alertDialog.setView(convertView);
+                alertDialog.setTitle(item);
+                ListView lv = (ListView) convertView.findViewById(R.id.listView1);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(CounterListActivity.this,android.R.layout.simple_list_item_1,names);
+                lv.setAdapter(adapter);
+                alertDialog.show();
+
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View viewIn, int position, long id) {
+                        Log.d("test","Item "+ item +": " + ((TextView) viewIn).getText());
+                        if(position == 0) //open button
+                        {
+
+                        }
+                        else if(position == 1) //rename button
+                        {
+
+                        }
+                        else if(position==2) //delete button
+                        {
+
+                        }
+                        else if(position==3) //details button
+                        {
+
+                        }
+                    }
+                });
+
+                return true;
             }
         });
     }
@@ -151,11 +198,26 @@ public class CounterListActivity extends AppCompatActivity {
             builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+
                     String it = input.getText().toString(); //input text
-                    multicounterNameList.add(it);
-                    saveCounterList(multicounterNameList);
-                    finish();
-                    startActivity(getIntent());
+
+                    if(it.equals(""))
+                    {
+                        Snackbar.make(getWindow().getDecorView().getRootView(), R.string.no_counter_name, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        dialog.cancel();
+                    }
+                    else if(inCounterList(it))
+                    {
+                        Snackbar.make(getWindow().getDecorView().getRootView(), R.string.counter_already_exists, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                        dialog.cancel();
+                    }
+                    else
+                    {
+                        multicounterNameList.add(it);
+                        saveCounterList(multicounterNameList);
+                        finish();
+                        startActivity(getIntent());
+                    }
 
                 }
             });
@@ -216,6 +278,18 @@ public class CounterListActivity extends AppCompatActivity {
         editor.putString(filename, jsonMoves);
         editor.commit();
     */
+
+   public boolean inCounterList(String counterName)
+   {
+       for(String s: multicounterNameList)
+       {
+           if(counterName.equals(s))
+           {
+               return true;
+           }
+       }
+       return false;
+   }
 
     public class OnSpinnerItemClicked implements AdapterView.OnItemSelectedListener {
 
